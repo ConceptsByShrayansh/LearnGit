1e4383eceea6e359fe73f2b775298947870b368b
Shrayansh <madhuramcoaching@gmail.com>
2026-07-18 01:29:22 +0530

    devb changes

--- src/main/java/com/concepts/Main.java
+++ src/main/java/com/concepts/Main.java
@@ -3,7 +3,7 @@ package com.concepts;
 public class Main {
     public static void main (String args[]) {
 
-        System.out.println("hello world! new changes");
+        System.out.println("hello world! new changes, code changes");
     }
 }
 
